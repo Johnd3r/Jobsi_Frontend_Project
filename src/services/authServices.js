641887f9e844eef80d3ff995d2e739c9818fdf1b
Johnd3r <johndernaranjo@gmail.com>
2026-02-25 18:50:26 -0500

    fix: Funcion de mensaje error mejorada
    - Se implemento una mejora al mensaje de error al crear cuenta con respecto a conflictos

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -7,11 +7,16 @@ export const register = async (data) => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
     });
+    //se crea variable que recibe la respuesta del servidor a la creacion de la cuenta
+    const responseData = await response.json();
 
-    if (!response.ok) throw new Error("Error registrando usuario");
-    return response.json();
-    };
-
+    if (!response.ok) {
+        //mensaje del backend
+        throw new Error(responseData.message || "Error registrando usuario");
+    }
+    // La variable recibe el mensaje y retorna el dato obtenido
+    return responseData;
+};
 export const login = async (email, password) => {
     const response = await fetch(`${AUTH_API}/login`, {
         method: "POST",
